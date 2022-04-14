a07e8d4e9e53563696ee9676b03f361e5db97820
davidktlee <rudxor567@naver.com>
2022-04-14 17:22:13 +0900

    refactor: 조건문으로 만들어서 길었던 문장 단순하게 만들기

--- main.js
+++ main.js
@@ -1,24 +1,8 @@
-let isHide = true
-
 function menuAdd(e) {
-  if (isHide) {
-    // 보임
-    isHide = !isHide
-    e.path[0].classList.remove('hide')
-  } else {
-    isHide = !isHide
-    e.path[0].classList.add('hide')
-  }
+  e.path[0].classList.remove('hide')
 }
-// 숨김
 function menuHide(e) {
-  if (!isHide) {
-    isHide = !isHide
-    e.path[0].classList.add('hide')
-  } else {
-    isHide = !isHide
-    e.path[0].classList.remove('hide')
-  }
+  e.path[0].classList.add('hide')
 }
 const menuEl = document.querySelector('.menu')
 menuEl.addEventListener('mouseover', menuAdd)
